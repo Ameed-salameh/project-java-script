df14cead5635c5322dd8dd6958923775b1027c58
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-29 16:01:41 +0300

    Section 2: var, let, const

--- Part One/one.js
+++ Part One/one.js
@@ -1,4 +1,4 @@
-/*
+
 
 //Section 1: console.log & Comments
 
@@ -30,7 +30,7 @@ console.log("5" + 5);
 console.log(10 + 20 + "30");
 
 
-*/
+
 
 //Section 2: var, let, const
 
@@ -54,3 +54,23 @@ console.log(x);
 
 
 const PI = 3.14;
+//PI = 3.1415; // ممنوع لانه const ما بزبط اغير عليه 
+
+
+
+if (true) {
+  var a = 10;
+}
+
+console.log(a); // بشتغل
+
+
+if (true) {
+  let b = 20;
+}
+
+console.log(b); //لا يعمل 
+
+
+const city = "Paris";
+//city = "London";// ممنوع لانه const ما بزبط اغير عليه 
