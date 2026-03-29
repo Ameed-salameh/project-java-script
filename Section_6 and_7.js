0a0b497aca5430e4ffca20d2bfb0202930d0228d
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-29 16:20:05 +0300

    Section 6

---
+++ Section_6 and_7.js
@@ -0,0 +1,21 @@
+console.log(5 == "5");   // true
+console.log(5 === "5");  // false
+
+
+let num = 30;
+
+if (num > 10 && num < 50) {
+  console.log("Valid");
+}
+
+
+
+console.log(10 != "10");
+console.log(10 !== "10");
+
+
+
+//Section 7: Logical & String Operators
+
+
+
