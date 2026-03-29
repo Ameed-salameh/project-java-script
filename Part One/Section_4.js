14b506d050ea5699ba8f88824f6e8766057916c7
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-29 16:11:27 +0300

    Section 4

---
+++ Part One/Section_4.js
@@ -0,0 +1,34 @@
+console.log((15 + 3) * 2 - 8 / 4);
+
+
+let num = 6;
+
+if (num % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+
+
+let a = 10;
+let b = 5;
+
+console.log("Sum:", a + b);
+console.log("Difference:", a - b);
+console.log("Product:", a * b);
+
+
+console.log(5 + 10 * 2);
+
+
+let x = 5;
+
+//  1
+x = x + 1;
+
+//  2
+x += 1;
+
+//  3
+x++;
