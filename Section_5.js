7a5e41b596ff5ef1ceca19c725667666e6d266b3
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-29 16:14:05 +0300

    Section 5

---
+++ Section_5.js
@@ -0,0 +1,35 @@
+let num = 10;
+
+num += 5;  // 15
+num -= 3;  // 12
+num *= 2;  // 24
+num /= 4;  // 6
+num %= 5;  // 1
+
+console.log(num);
+
+
+
+let x = 5;
+
+x += 10;  // 15
+x -= 3;   // 12
+x *= 2;   // 24
+
+console.log(x);
+
+
+
+let text = "Hello ";
+text += "World";
+
+console.log(text);
+
+
+
+
+let h = 4;
+
+h **= 2;
+
+console.log(h);
