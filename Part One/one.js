f352194f97def228294e29f5f22c994c7b6fa681
Ameed-salameh <ameed.salame123@gmail.com>
2026-03-29 12:50:10 +0300

    Section 1: console.log & Comments

--- Part One/one.js
+++ Part One/one.js
@@ -1,2 +1,24 @@
 console.log("Hello JavaScript World");
 
+
+// هذا كود لطباعة رسالة
+console.log("Hello JavaScript World");
+
+
+
+/*
+هذا كود بسيط
+يقوم بطباعة رسالة
+في الـ console
+*/
+console.log("Hello JavaScript World");
+
+
+let name = "Ameed";
+let age = 23;
+let country = "Palestine";
+
+console.log(name, age, country);
+
+
+
